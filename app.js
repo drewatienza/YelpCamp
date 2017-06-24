a446e7d49f2c015a605f5438b7d07cb9fb944f46
drewatienza <atienza.drew@gmail.com>
2017-06-24 16:06:35 -0700

    Added a show route/template

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 /*Campground.create({
     name: "Granite Hill", 
-    image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg"
+    image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg",
+    description: "This is a huge granite hill.  No bathrooms.  No water. Beautiful granite!"
 }, function(err, campground) {
     if(err) {
         console.log(err);
@@ -26,6 +28,68 @@ var Campground = mongoose.model("Campground", campgroundSchema);
         console.log(campground);
     }
 });*/
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+// INDEX - show all campgrounds
+app.get("/campgrounds", function(req, res) {
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
+});
+
+// CREATE - add new campground to DB
+app.post("/campgrounds", function(req, res) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+// NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs");
+});
+
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    // find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log(err);
+        } else {
+            // render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("The YelpCamp Server Has Started!");
+});
+
+
+
+
+
+
 
 /*var campgrounds = [
     {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4137/4812576807_8ba9255f38.jpg"},
@@ -38,42 +102,3 @@ var Campground = mongoose.model("Campground", campgroundSchema);
     {name: "Granite Hill", image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg"},
     {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"}
 ];*/
-
-app.get("/", function(req, res) {
-    res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res) {
-    // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
-        }
-    });
-});
-
-app.post("/campgrounds", function(req, res) {
-    // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    // create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated) {
-        if(err) {
-            console.log(err);
-        } else {
-            // redirect back to campgrounds page
-            res.redirect("/campgrounds");
-        }
-    });
-});
-
-app.get("/campgrounds/new", function(req, res) {
-    res.render("new.ejs");
-});
-
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
-    console.log("The YelpCamp Server Has Started!");
-});
