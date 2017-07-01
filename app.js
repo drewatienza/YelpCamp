dad725d8071c26f974ace09dc0bb6ca028b9d4c8
drewatienza <atienza.drew@gmail.com>
2017-07-01 13:46:35 -0700

    Added route to post new comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     seedDB = require("./seeds");
 
 seedDB();
@@ -76,6 +77,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
             res.render("comments/new", {campground: campground});
         }
     });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+    // look up campground using ID
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if(err) {
+                    console.loe(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    // create new comment
+    // connect new comment to campground
+    // redirect to campground show page
 });
 
 
