251a1174fd572e4c19db7b0700ca303342257c03
drewatienza <atienza.drew@gmail.com>
2017-07-04 14:45:02 -0700

    Configured passport and added register route and template

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ var express = require("express"),
     User = require("./models/user"),
     seedDB = require("./seeds");
 
-seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategry(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -100,6 +112,30 @@ app.post("/campgrounds/:id/comments", function(req, res) {
                 }
             });
         }
+    });
+});
+
+
+// ===========
+// AUTH ROUTES
+// ===========
+
+// show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
     });
 });
 
