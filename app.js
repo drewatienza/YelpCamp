fc37cb8171ff851f65e65cf031d3ae855efeb357
drewatienza <atienza.drew@gmail.com>
2017-07-10 15:28:10 -0700

    Added mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://primo6150:droopy27@ds153652.mlab.com:53652/drewyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
